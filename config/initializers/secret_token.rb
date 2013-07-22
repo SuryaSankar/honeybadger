44de6be12d6f11f6834e17d22faa7d1afc56807c
SuryaSankar <suryashankar.m@gmail.com>
2013-07-22 19:32:59 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Honeybadger::Application.config.secret_key_base = '9ab440f6fcbeae1d1bc91a00ac50be7bd363403d65dee75c36faac1be7475e1626c028d24dab6f06b150fbeb617ded3208915b8127749ea1b1f53b2c10590923'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Honeybadger::Application.config.session_store :cookie_store, key: '_honeybadger_session'
