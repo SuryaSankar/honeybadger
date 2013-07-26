b601244c716fba325e0a5c00c33a23e9e6215f8a
SuryaSankar <suryashankar.m@gmail.com>
2013-07-26 11:14:01 +0530

    made forms work

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Honeybadger::Application.routes.draw do
+  root 'pages#home'
+  get "pages/about"
+  resources :qpapers
+  resources :questions
+  resources :courses
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
