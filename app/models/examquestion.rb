0320d319d3d6da1e742f460c2946d14fb1e77766
SuryaSankar <suryashankar.m@gmail.com>
2013-08-06 18:59:36 +0530

    added reject if to examquestion. Qpaper works fine now

--- app/models/examquestion.rb
+++ app/models/examquestion.rb
@@ -1,5 +1,5 @@
 class Examquestion < ActiveRecord::Base
 	belongs_to :qpaper
 	belongs_to :question
-	accepts_nested_attributes_for :question
+	accepts_nested_attributes_for :question, reject_if: proc { |attributes| attributes['qtext'].blank? || attributes['qdesc'].blank? }
 end
