e19771fead7f7c45982b9525cd575de3b7f0f76b
SuryaSankar <suryashankar.m@gmail.com>
2013-08-26 03:33:48 +0530

    adding superscript to redcarpet

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
 	def render_markdown(text)
 		renderer=Redcarpet::Render::HTML.new(prettify: true, hard_wrap: true , filter_html: true)
-		extensions={ tables: true , footnotes: true, autolink: true, no_intra_emphasis: true , underline: true, quote: true}
+		extensions={ tables: true , footnotes: true, autolink: true, no_intra_emphasis: true , underline: true, quote: true, superscript: true, strikethrough: true}
 		Redcarpet::Markdown.new(renderer , extensions).render text	
 	end
 end
