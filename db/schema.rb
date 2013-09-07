8dea5160170fd516288cbc904c49c21db5d44b13
SuryaSankar <suryashankar.m@gmail.com>
2013-09-07 16:27:19 +0530

    removed fonts directory

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130828195631) do
+ActiveRecord::Schema.define(version: 20130907104445) do
 
   create_table "admins", force: true do |t|
     t.string   "email",              default: "", null: false
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 20130828195631) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.integer  "subquestion_no"
   end
 
   add_index "examquestions", ["qpaper_id"], name: "index_examquestions_on_qpaper_id"
