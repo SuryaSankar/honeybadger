cd0d28b93728d165c5ebda6c66fb1b8fe07b8013
SuryaSankar <suryashankar.m@gmail.com>
2013-10-09 20:01:01 +0530

    made tree links and removed prettify js

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,7 @@
 class PagesController < ApplicationController
 	def home
 		@show_branches_accordion=false
-		#gon.home_json=University.select([:id, :name]).includes(:branches, :programs => [:program_university_courses => { :university_course => :course }]).load.map{|u| {:label => u.name, :children => u.programs.to_a.map{ |p| { :label => p.shorter_desc , :children => (1..8).map{|x| { :label => "Semester "+x.to_s, :children => p.program_university_courses.select{ |puc| puc.semester==x && puc.university_course !=nil }.map{ |puc1| { :label => puc1.university_course.full_course_name } }  }} }} }}
-		@home_json=University.select([:id, :name]).includes(:branches, :programs => [:program_university_courses => { :university_course => :course }]).load.map{|u| {:label => u.name, :children => u.programs.to_a.map{ |p| { :label => p.shorter_desc , :children => (1..8).map{|x| { :label => "Semester "+x.to_s, :children => p.program_university_courses.select{ |puc| puc.semester==x && puc.university_course !=nil }.map{ |puc1| { :label => puc1.university_course.full_course_name } }  }} }} }}
+		@home_json=University.select([:id, :name]).includes(:branches, :programs => [:program_university_courses => { :university_course => :course }]).load.map{|u| {:label => u.name, :children => u.programs.to_a.map{ |p| { :label => p.shorter_desc , :children => (1..8).map{|x| { :label => "Semester "+x.to_s, :children => p.program_university_courses.select{ |puc| puc.semester==x && puc.university_course !=nil }.map{ |puc1| { :label => view_context.link_to(puc1.university_course.full_course_name, university_course_path(puc1.university_course_id) ) } }  }} }} }}
 		
 		render layout: "home_layout"
 	end
