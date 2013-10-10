9cfbf68f06f7252920cce5cbaa802943d38cae3e
SuryaSankar <suryashankar.m@gmail.com>
2013-10-10 19:31:05 +0530

    replaced jqtree with jstree

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,14 @@
 class PagesController < ApplicationController
 	def home
 		@show_branches_accordion=false
-		@home_json=University.select([:id, :name]).includes(:branches, :programs => [:program_university_courses => { :university_course => :course }]).load.map{|u| {:label => u.name, :children => u.programs.to_a.map{ |p| { :label => p.shorter_desc , :children => (1..8).map{|x| { :label => "Semester "+x.to_s, :children => p.program_university_courses.select{ |puc| puc.semester==x && puc.university_course !=nil }.map{ |puc1| { :label => view_context.link_to(puc1.university_course.full_course_name, university_course_path(puc1.university_course_id) ) } }  }} }} }}
+		@home_json=University.select([:id, :name]).includes(:branches, :programs => [:program_university_courses => { :university_course => :course }]).load.map{|u| {	"data" => u.name, 
+	"state" => "open", 
+	"children" => u.programs.to_a.map{ |p| { 
+			"data" => p.shorter_desc , 
+			"children" => (1..8).map{|x| { "data" => "Semester "+x.to_s, 
+							"children" => p.program_university_courses.select{ |puc| puc.semester==x && puc.university_course !=nil }.map{ |puc1| { 
+								"data" => { "title" => puc1.university_course.full_course_name, "attr" => { "href" => university_course_path(puc1.university_course_id) } }
+ } }  }} }} }}
 		
 		render layout: "home_layout"
 	end
@@ -13,3 +20,4 @@ class PagesController < ApplicationController
 	end
 	
 end
+
