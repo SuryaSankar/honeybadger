33a51fdcdc3bbf0fb36f08bd48c0c11e48c769a2
SuryaSankar <suryashankar.m@gmail.com>
2013-10-11 17:10:32 +0530

    modified some pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,7 +7,7 @@ class PagesController < ApplicationController
 			"data" => p.shorter_desc , 
 			"children" => (1..8).map{|x| { "data" => "Semester "+x.to_s, 
 							"children" => p.program_university_courses.select{ |puc| puc.semester==x && puc.university_course !=nil }.map{ |puc1| { 
-								"data" => { "title" => puc1.university_course.full_course_name, "attr" => { "href" => university_course_path(puc1.university_course_id) } }
+								"attr"=> { "rel" => "leaf" } ,"data" => { "title" => puc1.university_course.full_course_name, "attr" => { "href" => university_course_path(puc1.university_course_id) } }
  } }  }} }} }}
 		
 		render layout: "home_layout"
