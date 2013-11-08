5267c55a1ae7566e872f37471ce5d0e8fc2a9e0f
SuryaSankar <suryashankar.m@gmail.com>
2013-11-08 21:30:39 +0530

    added help texts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,6 +6,12 @@ module ApplicationHelper
 		Redcarpet::Markdown.new(renderer , extensions).render text	
 	end
 
+	def render_markdown_with_html(text)
+		renderer=Redcarpet::Render::HTML.new(prettify: true )
+		extensions={ tables: true , autolink: true, no_intra_emphasis: true , underline: true, quote: true,  strikethrough: true, fenced_code_blocks: true }
+		Redcarpet::Markdown.new(renderer , extensions).render text	
+	end
+
 	def roman_numeral(n)
 		romans=["i","ii","iii","iv","v","vi","vii","viii","ix","x","xi","xii","xiii","xiv","xv","xvi","xvii","xviii","xix","xx"]
 		n<=20 ? romans[n-1] : n.to_s
