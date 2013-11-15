95804968d7013a45697fa9cffdbab5ab462b46fe
SuryaSankar <suryashankar.m@gmail.com>
2013-11-15 14:34:01 +0530

    small changes

--- app/models/program.rb
+++ app/models/program.rb
@@ -2,7 +2,7 @@ class Program < ActiveRecord::Base
 	has_many :program_university_courses
 	has_many :university_courses, through: :program_university_courses
 	has_many :users
-	belongs_to :university
+	belongs_to :university, touch: true
 	belongs_to :branch
 	has_and_belongs_to_many :qpapers
 
--- app/models/program_university_course.rb
+++ app/models/program_university_course.rb
@@ -1,6 +1,6 @@
 class ProgramUniversityCourse < ActiveRecord::Base
-	belongs_to :university_course
-	belongs_to :program
+	belongs_to :university_course, touch: true
+	belongs_to :program, touch: true
 	accepts_nested_attributes_for :university_course,  reject_if: proc { |attributes| attributes['course_code'].blank?  }
 	validates_uniqueness_of :university_course_id, scope: [:program_id]
 
