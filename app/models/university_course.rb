9c61e96807e6725225dae716cd2d0cf6d41b9f18
SuryaSankar <suryashankar.m@gmail.com>
2013-11-22 19:38:59 +0530

    adding ce2402

--- app/models/university_course.rb
+++ app/models/university_course.rb
@@ -1,9 +1,9 @@
 class UniversityCourse < ActiveRecord::Base
-	belongs_to :university, touch: true	#EC321 belongs to NIT Trichy
-	belongs_to :course, touch: true	#EC321 belongs to Analog electronic circuits
-	has_many :qpapers	
-	has_many :program_university_courses, dependent: :destroy	#EC321 has 1.EC321 in B-Tech ECE, 2. EC321 in B-Tech EEE, 3. EC321 in B-Tech ICE
-	has_many :programs, through: :program_university_courses  #EC321 has B-Tech ECE, B-Tech EEE, M-Tech ICE
+	belongs_to :university, touch: true	
+	belongs_to :course, touch: true	
+	has_many :qpapers, after_remove: proc{ |a| a.touch }
+	has_many :program_university_courses, dependent: :destroy	
+	has_many :programs, through: :program_university_courses
 	has_many :units
 	has_and_belongs_to_many :outdated_courses, class_name: "UniversityCourse", foreign_key: "updated_course_id", join_table: "outdated_courses_updated_courses", association_foreign_key: "outdated_course_id", uniq: true
 	has_and_belongs_to_many :updated_courses, class_name: "UniversityCourse", foreign_key: "outdated_course_id", join_table: "outdated_courses_updated_courses", association_foreign_key: "updated_course_id", uniq: true	
