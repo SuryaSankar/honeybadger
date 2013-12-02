73cd7571bfa72cefe11e7e791eea38ae3aad5e93
SuryaSankar <suryashankar.m@gmail.com>
2013-12-02 23:45:28 +0530

    adding function for adding banners

--- app/models/textbook.rb
+++ app/models/textbook.rb
@@ -1,3 +1,7 @@
 class Textbook < ActiveRecord::Base
   belongs_to :university_course, touch: true
+
+  def self.createBook(university, university_course_code, book_name, banner)
+    Textbook.create name: book_name, university_course_id: UniversityCourse.find_by(university_id: University.find_by_name(university).id, course_code: university_course_code).id, flipkart_banner: banner
+  end
 end
